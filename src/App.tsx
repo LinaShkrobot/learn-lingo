50756f2403fb8fd55862c4b09f580a059b7d074b
Lina <linaskrobot@gmail.com>
2026-04-23 00:07:22 +0000

    fix loading flash on page refresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useAuth } from "./context/AuthContext";
 import HomePage from "./pages/HomePage";
 import TeachersPage from "./pages/TeachersPage";
 import FavoritesPage from "./pages/FavoritesPage";
 import PrivateRoute from "./components/PrivateRoute";
-import { Toaster } from "react-hot-toast";
 import Header from "./components/Header";
+import { Toaster } from "react-hot-toast";
 
 export default function App() {
+  const { loading } = useAuth();
+
+  if (loading) return <p>Loading...</p>;
+
   return (
     <BrowserRouter>
       <Header />
